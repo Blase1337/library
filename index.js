4bf508bf086b860fe3c8cb17f9d82af5b21a747f
Blase1337 <114041435+Blase1337@users.noreply.github.com>
2024-08-16 16:39:39 -0400

    Added remove button and toggle read button functionality

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read=false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 }
+
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
+};
 
 function addBookToLibrary(event) {
     event.preventDefault();
     const bookName = document.getElementById("bookName").value;
     const bookAuthor = document.getElementById("bookAuthor").value;
     const pageNumber = document.getElementById("pageNumber").value;
+
     let bookObject = new Book(bookName, bookAuthor, pageNumber);
     myLibrary.push(bookObject);
     const container = document.getElementById("main");
     container.innerHTML = '';
 
+    document.getElementById("bookName").value = '';
+    document.getElementById("bookAuthor").value = '';
+    document.getElementById("pageNumber").value = '';
+
     addToTable(myLibrary);
-
-
 }
 
 function addToTable(arr) {
     const container = document.getElementById("main");
-    arr.forEach(book =>{
+    container.innerHTML = '';
+
+    arr.forEach((book, index) =>{
         const newDiv = document.createElement('div');
-        newDiv.innerHTML = `Title: ${book.title}<br> <br> Author: ${book.author}<br> <br> Pages: ${book.pages}`;
+        newDiv.innerHTML = `Title: ${book.title}<br><br> Author: ${book.author}<br><br> Pages: ${book.pages}<br><br> Read: ${book.read ? "Yes" : "No"}<br><br>`;
         newDiv.classList.add('book-item');
+        //Remove Button
+        const newBtn = document.createElement('button');
+        newBtn.textContent = "Remove";
+        newBtn.type = "button";
+        newBtn.setAttribute('data-index', index);
+        newBtn.addEventListener("click", () => {
+            removeBook(index, newDiv);
+        });
+        //Toggle Read Button
+        const toggleReadBtn = document.createElement('button');
+        toggleReadBtn.type = "button";
+        toggleReadBtn.textContent = "Toggle Read";
+        toggleReadBtn.addEventListener("click", ()=>{
+            book.toggleReadStatus();
+            addToTable(arr)
+        });
+        
+        newDiv.appendChild(toggleReadBtn);
+        newDiv.appendChild(newBtn);
         container.appendChild(newDiv);
-    })
+    });
 }
 
-document.getElementById("closeForm").addEventListener("click", ()=>{
-        const form = document.getElementById("form");
-        form.style.display = "none"
-});
-
+function removeBook(index, element){
+    myLibrary.splice(index, 1); // Remove the book from the array
+    element.remove(); // Directly remove the corresponding DOM element
+    addToTable(myLibrary); // Re-render the table to ensure indices are updated
+}
 
 document.getElementById("addBookButton").addEventListener("click", addBookToLibrary);
 
@@ -48,6 +77,13 @@ document.getElementById("newBookButton").addEventListener("click", ()=>{
         form.style.display = "none";
     }
 });
+
+document.getElementById("closeForm").addEventListener("click", ()=>{
+        const form = document.getElementById("form");
+        form.style.display = "none"
+});
+
+
 
 
 
